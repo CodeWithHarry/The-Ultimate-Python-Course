d6be0dc2f82edc3a02c4e1eeb46521684cadb59d
Your Name <duachirag492@gmail.com>
2024-07-28 20:58:52 +0530

    this is yet another way

--- Chapter 3 - PS/02_problem2.py
+++ Chapter 3 - PS/02_problem2.py
@@ -2,4 +2,7 @@ letter = '''Dear <|Name|>,
 You are selected! 
 <|Date|> '''
 
-print(letter.replace("<|Name|>", "Harry").replace("<|Date|", "24 September 2050"))
+# print(letter.replace("<|Name|>", "Harry").replace("<|Date|", "24 September 2050"))
+var1=str(input("Enter the name here: "))
+var2=str(input("Enter the date here: "))
+print("Dear ",var1, "\nYou are selected!\n",var2)
