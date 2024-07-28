37d4e3d8ea4fa00e1735412206f65a5958781270
Your Name <duachirag492@gmail.com>
2024-07-28 21:05:52 +0530

    Revert "this is yet another way"
    This reverts commit d6be0dc2f82edc3a02c4e1eeb46521684cadb59d.

--- Chapter 3 - PS/02_problem2.py
+++ Chapter 3 - PS/02_problem2.py
@@ -2,7 +2,4 @@ letter = '''Dear <|Name|>,
 You are selected! 
 <|Date|> '''
 
-# print(letter.replace("<|Name|>", "Harry").replace("<|Date|", "24 September 2050"))
-var1=str(input("Enter the name here: "))
-var2=str(input("Enter the date here: "))
-print("Dear ",var1, "\nYou are selected!\n",var2)
+print(letter.replace("<|Name|>", "Harry").replace("<|Date|", "24 September 2050"))
