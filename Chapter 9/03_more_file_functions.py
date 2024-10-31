723a7a84fae979334e71c8240c52fae90add37a8
aakash-sachdev <aakashsachdev1992@gmail.com>
2024-10-30 19:33:58 -0500

    File IO and Practice

--- Chapter 9/03_more_file_functions.py
+++ Chapter 9/03_more_file_functions.py
@@ -1,25 +1,43 @@
-f = open("file.txt")
+f = open("filePractice.txt")
 
+# readlines returns the list of lines
 # lines = f.readlines()
 # print(lines, type(lines))
 
+print("----------------------------------------------------------------------------------------------------------")
+# This readline will give the line only
 # line1 = f.readline()
 # print(line1, type(line1))
+
+print("----------------------------------------------------------------------------------------------------------")
 
 # line2 = f.readline()
 # print(line2, type(line2))
 
+print("----------------------------------------------------------------------------------------------------------")
+
 # line3 = f.readline()
 # print(line3, type(line3))
+
+print("----------------------------------------------------------------------------------------------------------")
 
 # line4 = f.readline()
 # print(line4, type(line4))
 
+print("----------------------------------------------------------------------------------------------------------")
+
+# If there is no line then it will print empty string
 # line5 = f.readline()
 # print(line5 =="")
+
+print("----------------------------------------------------------------------------------------------------------")
+
+# Doing the same thing is loop so the it is easier to do-
 line = f.readline()
 while(line != ""):
     print(line)
     line = f.readline()
 
 f.close()
+
+print("----------------------------------------------------------------------------------------------------------")
--- Chapter 9/05_with.py
+++ Chapter 9/05_with.py
@@ -1,9 +1,14 @@
-f = open("file.txt")
-print(f.read())
-f.close()
+# a can be used instead of f but good practice is using f
+a = open("filePractice.txt")
+print(a.read())
+a.close()
 
+print("-----------------------------------------------------------------------------------")
+
+# with statement is mostly used
 # The same can be written using with statement like this:
-with open("file.txt") as f:
+
+with open("filePractice.txt") as f:
     print(f.read())
 
 # You dont have to explicitly close the file
